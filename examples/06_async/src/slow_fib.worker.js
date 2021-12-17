640df62e0b32a90ab5e721a6f3ee643415af42d9
daishi <daishi@axlight.com>
2021-12-17 18:37:38 +0900

    fix lint

--- examples/06_async/src/slow_fib.worker.js
+++ examples/06_async/src/slow_fib.worker.js
@@ -3,6 +3,6 @@ import { exposeWorker } from 'react-hooks-worker';
 const fib = async (i) => {
   await new Promise((r) => setTimeout(r, 10));
   return (i <= 1 ? i : await fib(i - 1) + await fib(i - 2));
-}
+};
 
 exposeWorker(fib);
