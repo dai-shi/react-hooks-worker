41c594a0acd5e0897dd3ac4d3f2ff17bbcf81793
Daishi Kato <dai-shi@users.noreply.github.com>
2021-12-17 18:40:40 +0900

    fix: handle nullish result from worker function (#36)
    * fix: handle nullish result from worker function
    
    * fix lint

--- src/exposeWorker.ts
+++ src/exposeWorker.ts
@@ -20,9 +20,9 @@
 export function exposeWorker(func: (data: any) => any) {
   self.onmessage = async (e: MessageEvent) => {
     const r = func(e.data);
-    if (r[Symbol.asyncIterator]) {
+    if (r && r[Symbol.asyncIterator]) {
       for await (const i of r) (self.postMessage as any /* FIXME */)(i);
-    } else if (r[Symbol.iterator]) {
+    } else if (r && r[Symbol.iterator]) {
       for (const i of r) (self.postMessage as any /* FIXME */)(i);
     } else {
       (self.postMessage as any /* FIXME */)(await r);
